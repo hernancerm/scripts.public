b70900cd4ad13544b68fe3c35675e34386cf0989
Hernán Cervera <hjcerveram@hotmail.com>
2022-03-12 23:29:00 -0600

    Create spub-git-delta-navigate to enforce proper loading of navigation pattern

---
+++ utils/delta-default-opts.sh
@@ -0,0 +1,8 @@
+DELTA_DEFAULT_OPTS=(\
+--syntax-theme='GitHub' \
+--file-style='bold blue' \
+--file-decoration-style='blue ul ol' \
+--hunk-header-style='syntax line-number' \
+--hunk-header-decoration-style='gray box' \
+--navigate)
+
