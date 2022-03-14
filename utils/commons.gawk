82464ede20541c0e134d6e40c43db12f1b3b66d8
Hernán Cervera <hjcerveram@hotmail.com>
2022-03-14 13:28:31 -0600

    Provide lib for manipulating standard directories (stdd)

--- utils/commons.gawk
+++ utils/commons.gawk
@@ -3,6 +3,10 @@
 # Credit of the `set_colors` function goes to DJ Mills (e36freak):
 # <https://github.com/e36freak/awk-libs/blob/master/colors.awk>
 
+
+# Styling
+# -------
+
 # Set colors and decorations (e.g. bold, italic), in the provided array.
 #
 # @param `array` the array in which to store colors and decorations.
@@ -48,18 +52,31 @@ function set_colors(array) {
   cmd | getline array["reset"];
   close(cmd);
 
-  # (Note on ansi escape codes
-  # --------------------------
+  # Note on ansi escape codes
+  # -------------------------
   # The ANSI escape code for italic text is: `ESC[3m`, where `ESC` is the ASCII
   # escape character. Every ANSI escape code begins with this `ESC`. In hex, this
-  # character is 1B, and the escape sequence `\x` in gawk, interprets hex
-  # digits as a character.
+  # character is 1B, and the escape sequence `\x` in gawk, interprets hex digits
+  # as a character.
+  #
   # <https://en.wikipedia.org/wiki/ANSI_escape_code>
-  # <https://en.wikipedia.org/wiki/Escape_character#ASCII_escape_character>).
+  # <https://en.wikipedia.org/wiki/Escape_character#ASCII_escape_character>.
 
   # Italic.
  array["italic"] = "\x1B[3m";
 }
+
+
+# Strings
+# -------
+
+function trim(string) {
+  return awk::gensub(/^\s+|\s+$/, "", "g", string);
+}
+
+
+# Date & time
+# -----------
 
 # @return today's date in ISO 8601 format (e.g. 2022-03-21).
 function get_today_date() {
---
+++ utils/stdd.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Manipulate «standard directories». These are directories which are
+# standard to my setup, and I commonly visit them. They are defined
+# in a CSV file. To learn what each column means, see the variable
+# `STDD__FIELD_OPTIONS`.
+
+declare -A STDD__FIELD_OPTIONS
+STDD__FIELD_OPTIONS=([path]=1 [description]=2 [metadata]=3)
+
+STDD__PUBLIC_FILEPATH='$SPUB/resources/stdd.pub.csv'
+STDD__PRIVATE_FILEPATH='$SPRI/resources/stdd.pri.csv'
+
+# @return public standard directories as-are, no transormations applied to file.
+function stdd__get_pub_raw {
+  cat "$(eval echo $STDD__PUBLIC_FILEPATH)"
+}
+
+# @return private standard directories as-are, no transormations applied to file.
+function stdd__get_pri_raw {
+  cat "$(eval echo $STDD__PRIVATE_FILEPATH)"
+}
+
+# @return all standard directories as-are, no transormations applied to the files.
+#         (the public directories are listed first, then the private ones).
+function stdd__get_all_raw {
+  stdd__get_pub_raw
+  stdd__get_pri_raw
+}
+
+# @stdin valid raw line(s) of standard directory(ies).
+# @param `$1` name of field to retrieve: `path`, `description` or `metadata`.
+# @return the trimmed value of the field.
+function stdd__get_field_from_raw {
+  local stdin="$(cat -)"
+  local field_value="$(echo "$stdin" \
+      | gawk -F',' -i 'commons.gawk' -v field="${STDD__FIELD_OPTIONS[$1]}" \
+          '{ print c::trim($field); }')"
+  echo "$field_value"
+}
+
+# Pretty print the provided lines of standard directories. This adds any styling
+# useful for a pleasing, human-readable presentation.
+#
+# @stdin valid line(s) of standard directory(ies).
+# @return pretty-printed standard directories.
+function stdd__pretty_print {
+  local stdin="$(cat -)"
+  filtered_field_values="$(echo $stdin | gawk -F',' '{ print $1", --"$2 }')"
+  echo "$filtered_field_values" | column -ts ','
+}
+
+# @stdin prettified line(s) of standard directory(ies).
+# @param `$1` name of field to retrieve: `path` or `description`.
+# @return the trimmed value of the field.
+function stdd__get_field_from_pretty {
+  local stdin="$(cat -)"
+  local field_value="$(echo "$stdin" \
+      | gawk -F'--' -i 'commons.gawk' -v field="${STDD__FIELD_OPTIONS[$1]}" \
+          '{ print c::trim($field); }')"
+  echo "$field_value"
+}
+
